093df916478fdef4416582dac09ae49661ee796b
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-03-24 11:07:27 +0500

    Update startup.lua

--- .nvim-personal/startup.lua
+++ .nvim-personal/startup.lua
@@ -1,3 +1,4 @@
+vim.fn.execute([[term explorer http://localhost:8000/en.html]])
 vim.fn.execute([[term explorer http://localhost:8000]])
 vim.fn.execute([[term .venv\Scripts\livereload.exe --host localhost --port 8000 docs]])
 vim.fn.execute([[e pages/en.md]])
