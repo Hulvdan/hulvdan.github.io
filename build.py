9a2cf0eeac896da72742f2657376d8a63262d2e5
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-04-28 21:12:27 +0500

    upd

--- build.py
+++ build.py
@@ -94,7 +94,7 @@ def process_line(line: str) -> str:
 
     if line.startswith("IMAGES "):
         images = [i.strip() for i in line.removeprefix("IMAGES ").split() if i]
-        line = """<div data-nanogallery2='{{"thumbnailWidth": "150", "thumbnailHeight": "100","thumbnailAlignment": "left", "thumbnailOpenImage": true}}'>{}</div>"""
+        line = """<div data-nanogallery2='{{"thumbnailWidth": "150", "thumbnailHeight": "100","thumbnailAlignment": "left", "thumbnailOpenImage": true, "thumbnailHoverEffect2": "imageScale150"}}'>{}</div>"""
         line = line.format(
             "".join(
                 '<a href="assets/{}" data-ngthumb="assets/th__{}.jpg"></a>'.format(
