b502490979989eecd53fb140df8825501050d88b
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-04-30 00:30:40 +0500

    upd

--- .nvim-personal/startup.lua
+++ .nvim-personal/startup.lua
@@ -1,7 +1,7 @@
 -- vim.fn.execute([[term explorer http://localhost:8000/en.html]])
 vim.fn.execute([[term explorer http://localhost:8000]])
 vim.fn.execute([[term .venv\Scripts\livereload.exe --host localhost --port 8000 docs]])
-vim.fn.execute([[e pages/en.md]])
+-- vim.fn.execute([[e pages/en.md]])
 vim.fn.execute([[e pages/index.md]])
 
 vim.defer_fn(function()
